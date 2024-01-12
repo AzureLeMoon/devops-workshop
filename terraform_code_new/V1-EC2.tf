f84983814e51d7667ee5fd0ed8d4d0e993ca36fc
AzureleMoon <62159058+AzureLeMoon@users.noreply.github.com>
2024-01-12 12:34:59 +0330

    commiting v1-ec2.tf

---
+++ terraform_code_new/V1-EC2.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.32.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0c7217cdde317cfec"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+}
